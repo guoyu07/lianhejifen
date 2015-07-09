4a6af7873ca55500e45b994cbf1a62194a4c8f2f
Aijifen <arongkuaile@163.com>
2015-07-09 09:32:38 +0800

    更改该花积分详情页面

--- 联合积分/Views/view/ChooseView.m
+++ 联合积分/Views/view/ChooseView.m
@@ -9,39 +9,127 @@
 #import "ChooseView.h"
 
 @implementation ChooseView
+{
+    NSMutableArray *_labelArr;
+    UITextField    *_quantityTextField;
+    
+    NSMutableDictionary *_buttonDic;
+}
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
 //        [self createUI];
+        _labelArr = [[NSMutableArray alloc] init];
+        _buttonDic = [[NSMutableDictionary alloc] init];
     }
     return self;
 }
 
 - (void)createUI {
-        
+    /***********创建标题显示**************/
     for (int i=0; i<self.nameArray.count; i++) {
-        NSArray *currentArray = self.contentArray[i];
-        NSInteger indexRow = currentArray.count/3;
-        UILabel *nameLabel = [GZRControl createLabelWithFrame:CGRectMake(10, 30+(40+5+5)*indexRow*i, 50, 20) Font:15 Text:[self.nameArray objectAtIndex:i] TextColor:[UIColor blackColor] TextAligent:NSTextAlignmentLeft];
+        NSArray *currentArray;
+        if (i>0) {
+            currentArray = self.contentArray[i-1];
+        }
+        NSInteger indexRow;
+        if (currentArray.count <= 3) {
+            indexRow = 1;
+        } else if (currentArray.count > 3 && currentArray.count <= 6) {
+            indexRow = 2;
+        }
+        UILabel *nameLabel = [GZRControl createLabelWithFrame:CGRectMake(10, 30+(20+(40*indexRow+5+5))*i, 50, 20) Font:15 Text:[self.nameArray objectAtIndex:i] TextColor:[UIColor blackColor] TextAligent:NSTextAlignmentLeft];
+        if (i>0) {
+            UILabel *currentLabel = _labelArr[i-1];
+            nameLabel.frame = CGRectMake(10, CGRectGetMaxY(currentLabel.frame)+40*indexRow+5*(indexRow+1), 50, 20);
+        }
         [self addSubview:nameLabel];
+        [_labelArr addObject:nameLabel];
         
     }
     
+    /*************创建选择按钮***************/
     NSInteger btnW = (SCREEN_WIDTH-30)/3;
-    for (NSArray *currentArr in self.contentArray) {
+    NSInteger row = self.contentArray.count;
+    for (int j=0; j<row; j++) {
+        NSArray *currentArr = self.contentArray[j];
+        UILabel *currentLabel = [_labelArr objectAtIndex:j];
         for (int i=0; i<currentArr.count; i++) {
-            UIButton *button = [GZRControl createButtonWithFrame:CGRectMake(10+i%3*(5+btnW), 55+i/3*(5+40), btnW, 40) ImageName:nil Target:self Action:@selector(buttonClicked:) Title:[currentArr objectAtIndex:i] titleColor:[UIColor blackColor] backColor:[UIColor whiteColor] cornerRadius:0 masks:NO];
+            UIButton *button = [GZRControl createButtonWithFrame:CGRectMake(10+i%3*(5+btnW), (CGRectGetMaxY(currentLabel.frame)+5)+i/3*(5+40), btnW, 40) ImageName:nil Target:self Action:@selector(buttonClicked:) Title:[currentArr objectAtIndex:i] titleColor:[UIColor blackColor] backColor:[UIColor whiteColor] cornerRadius:0 masks:NO];
+            [button setBackgroundImage:[UIImage imageNamed:@"btnBg"] forState:UIControlStateSelected];
             button.layer.borderColor = [[UIColor grayColor] CGColor];
             button.layer.borderWidth = 1.0;
+            button.tag = 111 + i;
+            [_buttonDic setValue:button forKey:[NSString stringWithFormat:@"%d",j]];
             [self addSubview:button];
         }
+    }
+    
+    /**************创建数量选择视图****************/
+    UILabel *currentLabel = [_labelArr lastObject];
+    NSArray *symbolArr  = [NSArray arrayWithObjects:@"-",@"+", nil];
+    for (int i=0; i<2; i++) {
+        UIButton *calculateBtn = [GZRControl createButtonWithFrame:CGRectMake(10+(50+80)*i, CGRectGetMaxY(currentLabel.frame)+5, 50, 40) ImageName:nil Target:self Action:@selector(calculateBtnClicked:) Title:[symbolArr objectAtIndex:i] titleColor:[UIColor blackColor] backColor:RGBCOLOR(227, 228, 229) cornerRadius:0 masks:NO];
+        [calculateBtn setTitleColor:RGBCOLOR(228, 35, 117) forState:UIControlStateHighlighted];
+        calculateBtn.layer.borderColor = [[UIColor grayColor] CGColor];
+        calculateBtn.layer.borderWidth = 1.0;
+        calculateBtn.tag = 999 + i;
+        [self addSubview:calculateBtn];
+    }
+    
+    _quantityTextField = [[UITextField alloc] initWithFrame:CGRectMake(60, CGRectGetMaxY(currentLabel.frame)+5, 80, 40)];
+    _quantityTextField.userInteractionEnabled = NO;
+    _quantityTextField.layer.borderColor = [[UIColor grayColor] CGColor];
+    _quantityTextField.layer.borderWidth = 1.0;
+    _quantityTextField.backgroundColor = [UIColor whiteColor];
+//    _quantityTextField.placeholder = @"1";
+    _quantityTextField.textAlignment = NSTextAlignmentCenter;
+    _quantityTextField.text = @"1";
+    [self addSubview:_quantityTextField];
+    
+    UIButton *confirmBtn = [GZRControl createButtonWithFrame:CGRectMake(0, CGRectGetMaxY(_quantityTextField.frame)+20, SCREEN_WIDTH, 49) ImageName:nil Target:self Action:@selector(confirmBtnClicked) Title:@"确 定" titleColor:[UIColor whiteColor] backColor:RGBCOLOR(228, 35, 117) cornerRadius:0 masks:NO];
+    [self addSubview:confirmBtn];
+    
+    if ([self.delegate respondsToSelector:@selector(makeViewRectY:)]) {
+        [self.delegate makeViewRectY:CGRectGetMaxY(confirmBtn.frame)];
     }
 }
 
 - (void)buttonClicked:(UIButton *)btn {
+    for (UIButton *currentBtn in _buttonDic) {
+        
+    }
     
+    NSInteger indexSum = self.contentArray.count;
+    for (int j=0; j<indexSum; j++) {
+        UIButton *currentBtn = [_buttonDic valueForKey:[NSString stringWithFormat:@"%d",j]];
+        if (currentBtn.tag == btn.tag) {
+            currentBtn.selected = YES;
+        } else {
+            currentBtn.selected = NO;
+        }
+    }
 }
+
+- (void)calculateBtnClicked:(UIButton *)btn {
+    if (btn.tag-999 == 0) { // 数量减1
+        if ([_quantityTextField.text intValue] > 0) { // 判断数量是否大于零
+            _quantityTextField.text = [NSString stringWithFormat:@"%d",[_quantityTextField.text intValue]-1];
+        }
+    } else {    // 数量加1
+        _quantityTextField.text = [NSString stringWithFormat:@"%d",[_quantityTextField.text intValue]+1];
+    }
+}
+
+// 确定并提交所选择的信息字典
+- (void)confirmBtnClicked {
+#warning 此处需要判断用户是否选择了商品类别，如果没有选择需要提醒用户
+    if ([self.delegate respondsToSelector:@selector(confirmAndgetInformation:)]) {
+        [self.delegate confirmAndgetInformation:nil];
+    }
+}
+
 
 /*
 // Only override drawRect: if you perform custom drawing.
